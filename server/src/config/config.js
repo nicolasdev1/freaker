d355e18aadfb995f12668e3b69d871b0652354cb
nicolasdev1 <nicolaspessoal@icloud.com>
2020-10-11 15:26:21 -0300

    Change database from MySQL to PostgreSQL

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-  dialect: 'mysql',
+  dialect: 'postgres',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   username: process.env.DB_USER,
