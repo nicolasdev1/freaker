574fa6dc58df9deef0726ead8de886cc9344b5e7
alexandrevelozo <alexandrevelozo4@gmail.com>
2020-11-04 17:52:18 -0300

    pivot table implementation for orders

---
+++ server/src/database/migrations/20201102233535-create-order-item.js
@@ -0,0 +1,42 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('order_items', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      order_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'orders', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      product_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'products', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: 'created_at'
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: 'updated_at'
+      }
+    })
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('order_items')
+  }
+}
