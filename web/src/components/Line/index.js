c23bd0a62f35a0e7fd02a3834cd33566945e27db
alexandrevelozo <alexandrevelozo4@gmail.com>
2020-11-07 19:29:37 -0300

    create component Line

---
+++ web/src/components/Line/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import styled from 'styled-components'
+
+const Div = styled.div`
+  margin-top: 50px;
+  border-bottom: 1px solid #d3d3d3;
+`
+
+const Line = () => {
+  return <Div />;
+};
+
+export default Line;
