102f65c97c467cc0ac0e1036c9ab7b1b8fc2fc57
alexandrevelozo <alexandrevelozo4@gmail.com>
2020-11-07 19:28:11 -0300

    started form users

--- web/src/pages/Users/index.js
+++ web/src/pages/Users/index.js
@@ -7,12 +7,18 @@ import NavBar from '../../components/NavBar'
 import Status from '../../components/Status'
 import User from '../../components/User'
 import Search from '../../components/Search'
+import Line from '../../components/Line'
+import Footer from '../../components/Footer'
 
 import api from '../../services/api'
 
 import {
   Title,
   Grid,
+  Form,
+  Wrapper,
+  Enter,
+  ButtonContainer,
 } from './styles'
 
 const Users = () => {
@@ -72,6 +78,72 @@ const Users = () => {
           )
         }
       </Grid>
+
+      <Line />
+
+      <Form onSubmit={() => {}}>
+          <h1>Criar um novo cliente</h1>
+          <p>Forneça os dados necessários para cadastrar um cliente</p>
+
+        <Wrapper>
+          <div>
+            <label htmlFor="name">Nome do cliente</label>
+            <input
+              type="text"
+              // value={name}
+              // onChange={(event) => setName(event.target.value)}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="images">Endereço</label>
+            <input
+              type="text"
+              // value={image}
+              // onChange={(event) => setImage(event.target.value)}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="cost_price">Rua</label>
+            <input
+              type="number"
+              // value={cost_price}
+              // onChange={(event) => setCostPrice(event.target.value)}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="sale_price">Preço de venda</label>
+            <input
+              type="number"
+              // value={sale_price}
+              // onChange={(event) => setSalePrice(event.target.value)}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="stock">Estoque</label>
+            <input
+              type="number"
+              // value={stock}
+              // onChange={(event) => setStock(event.target.value)}
+            />
+          </div>
+          </Wrapper>
+
+          <ButtonContainer>
+            <Enter type="submit">
+              <span>
+                <img src="/images/icons/enter.svg" alt="Entrar" />
+              </span>
+
+              <strong>Cadastrar produto</strong>
+            </Enter>
+          </ButtonContainer>
+
+        </Form>
+        <Footer />
     </Container>
   )
 }
